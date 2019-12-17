07b66f4820c4c2c3920d2972cbd5abdbed46457f
Antero Marjamäki <antero.marjamaki@gmail.com>
2019-12-17 23:41:26 +0200

    Local stiffness matrix testing, next transfer that to eigen

--- test/Spec.hs
+++ test/Spec.hs
@@ -28,28 +28,33 @@ makeMesh = do
    mesh <- mkMesh <$> gmshReadMeshData
    return mesh
 
+foreach = flip map
+
+cartProduct xs ys = [(x,y) | x <- xs, y <- ys]
+
+gaussIntegrate ips f = sum $ map (\(ip, iw)-> f(ip) * iw) ips
+
+makeLocalStiffMat :: Element -> [(NodeTag, NodeTag, Double)]
+makeLocalStiffMat element = map fun dofs
+  where  
+    intpoints = gaussIntegrationScheme element 1
+    ntags = zip [0..] $ elementNodeTags element
+    dofs = cartProduct ntags ntags
+    basf i = nodeScalarBasis element i
+    linearForm i j p = (basf i p)*(basf j p)
+    fun ((i,gi), (j,gj)) = (gi, gj, gaussIntegrate intpoints $ linearForm i j) 
+    
+makeStiffnessMatrix :: Domain -> [(NodeTag, NodeTag, Double)]
+makeStiffnessMatrix  = concat . map makeLocalStiffMat . domainElements
+    
+
 main :: IO ()
 main = do
-   putStrLn ""
-   putStrLn "Running test code..."
-   gmshInitialize [] Nothing
-   gmshModelOccAddRectangle 0 0 0 1 1 Nothing Nothing
-   gmshModelOccAddRectangle 1 1 0 1 1 Nothing Nothing
-   gmshModelOccAddRectangle (-1) (-1) 0 1 1 Nothing Nothing
-
-   gmshModelOccSynchronize
-   points <- gmshModelGetEntities (Just 0)
-   gmshModelOccSetMeshSize points 1.0
-   gmshModelOccSynchronize
-   gmshModelAddPhysicalGroup 2 [1,2] Nothing
-   gmshModelAddPhysicalGroup 2 [3] Nothing
-   gmshModelMeshGenerate $ Just 2
-
-   mesh <- mkMesh <$> gmshReadMeshData
+   mesh <- makeMesh
 
    let domain = defineDomain (DimTag <$> [(2,1), (2,2)]) mesh
    let field = fromFunction domain (\[x,y,z] -> y)
 
    gmshExportField domain (Just "Asd") field
-   gmshFltkRun                 
+--   gmshFltkRun                 
    gmshFinalize
